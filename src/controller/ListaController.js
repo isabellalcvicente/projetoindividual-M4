097c7d3b633b479985112eeaed278d4d44bf8e70
isabellalcvicente <isabellavicente@ufpr.br>
2023-09-09 20:27:28 -0300

    colocando o static

--- src/controller/ListaController.js
+++ src/controller/ListaController.js
@@ -3,3 +3,15 @@ const prompt = require('prompt-sync')();
 //importando a biblioteca prompt 
 
 
+class ListaController{
+    static criarLista(){
+        do {
+            var opcao = prompt("Digite qual propriedade você quer ")
+            //criando uma variavel unitaria que armazena a resposta do prompt (por tempo limitado)
+            lista.push(opcao)
+        } while(opcao !== 'sair');
+        //utilizando o do...while, pois a repetição deve ser realizada até o usuário digitar sair
+        //utilizei o do...while e não apenas o while, pois se utilizasse apenas o while ele teria que ser ativado, ou rodaria pela "metade"
+    }
+
+}
